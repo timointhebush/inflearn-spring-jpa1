7fddde74ffaf4486ceeb00366fc2ccc90005972c
timointhebush <junzzang97@naver.com>
2021-07-13 00:08:20 +0900

    개발환경 세팅 - H2DB

---
+++ src/main/java/jpabook/japshop/Hello.java
@@ -0,0 +1,9 @@
+package jpabook.japshop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    public String name;
+}
---
+++ src/main/java/jpabook/japshop/Member.java
@@ -0,0 +1,18 @@
+package jpabook.japshop;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+}
